847bd816d522e80ecdf03a9e8c853a05645dd23b
Victoria Santos <4surferwahine.bp@gmail.com>
2018-10-03 12:44:42 -0400

    Finished favorite food and password authentication

--- FavoriteFood/Program.cs
+++ FavoriteFood/Program.cs
@@ -6,9 +6,19 @@ namespace FavoriteFood
     {
         static void Main(string[] args)
         {
-            String answer = "";
-            Console.WriteLine("What's your favorite food?");
-            answer = Console.ReadLine();
+            String response = "";
+            Console.WriteLine("What is your favorite food?");
+            response = Console.ReadLine();
+            if (response.Equals("pizza"))
+            {
+                Console.WriteLine("Most excellent and correct! Pizza is indeed one of the best foods ever put on this earth.");
+            }
+            else
+            {
+                Console.WriteLine("Wrong! That is a terrible choice and you should feel bad about yourself. Your favorite should be pizza!");
+            }
+
+
         }
     }
 }
--- PasswordAuthentication/Program.cs
+++ PasswordAuthentication/Program.cs
@@ -6,7 +6,28 @@ namespace PasswordAuthentication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String userIn = "";
+            String passIn = "";
+            int attempts = 0;
+
+            while (attempts < 3)
+            {
+                Console.WriteLine("Enter a username.");
+                userIn = Console.ReadLine();
+                Console.WriteLine("Enter a password.");
+                passIn = Console.ReadLine();
+
+                if (userIn.Equals("root") && passIn.Equals("letmein"))
+                {
+                    Console.WriteLine("Username and password authenticated. You may proceed.");
+                    break;
+                }
+                else
+                {
+                    attempts++;
+                }
+            }
+            Console.WriteLine("Too many incorrect login attempts! Try again later!");
         }
     }
 }
